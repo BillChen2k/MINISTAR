d2a3c33d5f22b56588766d54e173c8e9d01fb2e7
LeoTovey <58586997+LeoTovey@users.noreply.github.com>
2022-06-07 21:16:25 +0800

    handle handinput in new input system

--- Assets/Script/HandInput.cs
+++ Assets/Script/HandInput.cs
@@ -38,6 +38,7 @@ public class HandInput : MonoBehaviour
     {
         Mouse mouse = Mouse.current; // 鼠标
         Touchscreen touchScreen = Touchscreen.current;
+        Pointer pointer = Pointer.current;
         ReadOnlyArray<TouchControl> touches = touchScreen.touches;
         if (mouse.leftButton.wasPressedThisFrame || touches[0].phase.ReadValue() == TouchPhase.Began)
         {
@@ -62,7 +63,8 @@ public class HandInput : MonoBehaviour
             //设置顶点数
             line.positionCount++;
             //Vector2 mousePosition = mouse.position.ReadValue();
-            Vector2 mousePosition = touches[0].position.ReadValue();
+            Vector2 mousePosition = pointer.position.ReadValue();
+            Debug.LogError(mousePosition);
             //设置顶点位置(顶点的索引，将鼠标点击的屏幕坐标转换为世界坐标)
             line.SetPosition(i - 1,  Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, 15)));
         }
